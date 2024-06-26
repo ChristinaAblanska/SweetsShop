497738788aaeaed502dcb0ed7308ab2bc6598267
MartinDoychev <doychev.martin@gmail.com>
2024-06-26 20:15:31 +0300

    Created entities

---
+++ src/main/java/com/academy/cakeshop/persistance/entity/Unit.java
@@ -0,0 +1,26 @@
+package com.academy.cakeshop.persistance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "units")
+public class Unit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "unit", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Product> products;
+}
